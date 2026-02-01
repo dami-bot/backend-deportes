993fe4154e97d05925cdac8447cf9953a50283ea
damian <damianl32cooking@gmail.com>
2026-02-01 19:05:09 -0300

    estructura inicial nest

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    // En Prisma 7, es OBLIGATORIO pasarle la configuración al super()
+    super({
+      
+       datasourceUrl: process.env.DATABASE_URL,
+      
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
